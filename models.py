e99ba5a5aa9e741928e86d176e24f5fa0e241d0a
hedge0 <eliasmemail@gmail.com>
2024-08-18 21:55:59 -0400

    changed func order

--- models.py
+++ models.py
@@ -146,6 +146,28 @@ def compute_metrics(x, y_mid, model, params):
     
     return chi_squared, avE5
 
+def filter_strikes(x, S, num_stdev=1.25, two_sigma_move=False):
+    """
+    Filter strike prices around the underlying asset's price.
+
+    Args:
+        x (array-like): Array of strike prices.
+        S (float): Current underlying price.
+        num_stdev (float, optional): Number of standard deviations for filtering. Defaults to 1.25.
+        two_sigma_move (bool, optional): Adjust upper bound for a 2-sigma move. Defaults to False.
+
+    Returns:
+        array-like: Filtered array of strike prices within the specified range.
+    """
+    stdev = np.std(x)
+    lower_bound = S - num_stdev * stdev
+    upper_bound = S + num_stdev * stdev
+
+    if two_sigma_move:
+        upper_bound = S + 2 * stdev
+
+    return x[(x >= lower_bound) & (x <= upper_bound)]
+
 @njit
 def erf(x):
     """
@@ -336,25 +358,3 @@ def calculate_implied_volatility_lr(mid_price, S, K, r, T, option_type='calls', 
             upper_vol *= 2.0
 
     return mid_vol
-
-def filter_strikes(x, S, num_stdev=1.25, two_sigma_move=False):
-    """
-    Filter strike prices around the underlying asset's price.
-
-    Args:
-        x (array-like): Array of strike prices.
-        S (float): Current underlying price.
-        num_stdev (float, optional): Number of standard deviations for filtering. Defaults to 1.25.
-        two_sigma_move (bool, optional): Adjust upper bound for a 2-sigma move. Defaults to False.
-
-    Returns:
-        array-like: Filtered array of strike prices within the specified range.
-    """
-    stdev = np.std(x)
-    lower_bound = S - num_stdev * stdev
-    upper_bound = S + num_stdev * stdev
-
-    if two_sigma_move:
-        upper_bound = S + 2 * stdev
-
-    return x[(x >= lower_bound) & (x <= upper_bound)]
