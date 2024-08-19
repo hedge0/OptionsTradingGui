fixed normalization of model

plot_manager.py
@@ -190,24 +190,10 @@ class PlotManager:
             for line in self.lines:
                 line.remove()
 
-
-        print(x)
-        print(y_mid)
-
-
-        # ADD THE NORMALIZATION HERE
+        # normalize X values here
         scaler = MinMaxScaler()
-
-        # Normalize x to [0, 1]
         x_normalized = scaler.fit_transform(x.reshape(-1, 1)).flatten()
-
-        # Adjust the range to [1, 2] by adding 1
         x_normalized = x_normalized + 1
-
-        self.bids = self.ax.scatter(x_normalized, y_bid, color='red', s=10, label="Bid")
-        self.asks = self.ax.scatter(x_normalized, y_ask, color='red', s=10, label="Ask")
-        self.midpoints = self.ax.scatter(x_normalized, y_mid, color='red', s=20, label="Midpoint")
-        self.lines = [self.ax.plot([x_normalized[i], x_normalized[i]], [y_bid[i], y_ask[i]], color='red', linewidth=0.5)[0] for i in range(len(x))]
 
         model = {
             "SVI": svi_model,
@@ -218,17 +204,22 @@ class PlotManager:
 
         # Apply the selected objective function and fit the model
         params = fit_model(x_normalized, y_mid, y_bid, y_ask, model, method=self.selected_objective.get())
-        fine_x = np.linspace(np.min(x_normalized), np.max(x_normalized), 200)
-        interpolated_y = model(np.log(fine_x), params)
+        fine_x_normalized = np.linspace(np.min(x_normalized), np.max(x_normalized), 200)
+        fine_x = np.linspace(np.min(x), np.max(x), 200)
+        interpolated_y = model(np.log(fine_x_normalized), params)
+        chi_squared, avE5 = compute_metrics(x_normalized, y_mid, model, params)
+
+        # Apply results to plot
+        self.metrics_text.config(text=f"χ²: {chi_squared:.4f}    avE5: {avE5:.2f} bps")
+        self.bids = self.ax.scatter(x, y_bid, color='red', s=10, label="Bid")
+        self.asks = self.ax.scatter(x, y_ask, color='red', s=10, label="Ask")
+        self.midpoints = self.ax.scatter(x, y_mid, color='red', s=20, label="Midpoint")
+        self.lines = [self.ax.plot([x[i], x[i]], [y_bid[i], y_ask[i]], color='red', linewidth=0.5)[0] for i in range(len(x))]
 
         if hasattr(self, 'fit_line'):
             self.fit_line.set_data(fine_x, interpolated_y)
         else:
             self.fit_line, = self.ax.plot(fine_x, interpolated_y, color='green', label="Fit", linewidth=1.5)
-        
-        # Compute and display metrics
-        chi_squared, avE5 = compute_metrics(x_normalized, y_mid, model, params)
-        self.metrics_text.config(text=f"χ²: {chi_squared:.4f}    avE5: {avE5:.2f} bps")
 
         self.canvas.draw()
 